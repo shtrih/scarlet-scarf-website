61bee7337376fd5f1e90de237180a834d45cf53d
Dmitry M <d.morozov@resolutionpoint.ru>
2015-03-19 17:43:48 +0400

    Add dist/ directory

---
+++ dist/js/main.js
@@ -0,0 +1,46 @@
+$(function () {
+    $('article:not(.carousel) .bg').parallax({
+        start: 'elstart',
+        stop: 'elend',
+        speed: 0.1,
+        axis: 'y',
+        threshold: 100
+    });
+
+    $('.carousel ul').anoSlide({
+        items: 1,
+        speed: 900,
+        prev: 'a.prev',
+        next: 'a.next',
+        // lazy: true,
+        // auto: 4000,
+        onConstruct: function(instance) {
+            var paging = $('<div/>').addClass('paging fix').css({
+                position: 'absolute',
+                top: 1,
+                left:50 + '%',
+                width: instance.slides.length * 40,
+                marginLeft: -(instance.slides.length * 40) / 2
+            });
+
+            /* Build paging */
+            var onPagingClick = function () {
+                instance.stop().go($(this).data('index'));
+            };
+            for (var i = 0, l = instance.slides.length; i < l; i++) {
+                var a = $('<a/>').data('index', i).appendTo(paging).on('click', onPagingClick);
+
+                if (i === instance.current) {
+                    a.addClass('current');
+                }
+            }
+
+            instance.element.parent().append(paging);
+        },
+        onStart: function (ui) {
+            var paging = $('.paging');
+
+            paging.find('a').eq(ui.instance.current).addClass('current').siblings().removeClass('current');
+        }
+    });
+});

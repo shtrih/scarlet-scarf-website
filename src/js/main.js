73f44f5dcafa2930eb82aec1f9fdc709f5cd0a6e
Dmitry M <d.morozov@resolutionpoint.ru>
2015-04-06 15:28:55 +0400

    Styles, scripts

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ $(function () {
         easing:'easeInOutCirc'
     });
 
-    if (!($(document).width() < 1024 || $(document).height() < 640)) {
+    if (!($(document).width() <= 1024 || $(document).height() < 640)) {
         $.stellar({
             // horizontalOffset: 40,
             // verticalOffset: 0
@@ -19,34 +19,35 @@ $(function () {
         navigationItems = $('a', navigationBlock)
     ;
 
-    function updateNavigation() {
-        contentSections.each(function () {
-            var $this = $(this),
-                anchor = $('a[name]', $this),
-                activeSection = $('a[href="#' + anchor.attr('name') + '"]', navigationBlock).data('number') - 1
-            ;
+    if (!$('.touch').length) {
+        var updateNavigation = function () {
+            contentSections.each(function () {
+                var $this = $(this),
+                    anchor = $('a[name]', $this),
+                    activeSection = $('a[href="#' + anchor.attr('name') + '"]', navigationBlock).data('number') - 1
+                ;
 
-            if (($this.offset().top - $(window).height() / 2 < $(window).scrollTop()) && ($this.offset().top + $this.height() - $(window).height() / 2 > $(window).scrollTop())) {
-                navigationItems.eq(activeSection).addClass('is-selected');
-            } else {
-                navigationItems.eq(activeSection).removeClass('is-selected');
-            }
+                if (($this.offset().top - $(window).height() / 2 < $(window).scrollTop()) && ($this.offset().top + $this.height() - $(window).height() / 2 > $(window).scrollTop())) {
+                    navigationItems.eq(activeSection).addClass('is-selected');
+                } else {
+                    navigationItems.eq(activeSection).removeClass('is-selected');
+                }
+            });
+        };
+
+        updateNavigation();
+        $(window).on('scroll', function() {
+            updateNavigation();
         });
     }
 
-    updateNavigation();
-    $(window).on('scroll', function() {
-        updateNavigation();
-    });
-
     // open-close navigation on touch devices
     $('.touch .cd-nav-trigger').on('click', function () {
-        $('.touch #cd-vertical-nav').toggleClass('open');
-
+        navigationBlock.toggleClass('open');
     });
     // close navigation on touch devices when selectin an elemnt from the list
     $('.touch #cd-vertical-nav a').on('click', function () {
-        $('.touch #cd-vertical-nav').removeClass('open');
+        navigationBlock.removeClass('open');
     });
 
     $('.carousel ul').anoSlide({

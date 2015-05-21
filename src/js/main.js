5ce83ab9ba32b2f0585cd33eb21889c463ebac53
Dmitry M <s@hitagi.ru>
2015-05-21 16:18:45 +0400

    Вместо anoslide используем localScroll. Удаляю anoslide. Правки по стилям.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,17 @@
 ;(function($, window, document, undefined) {
     $.localScroll({
+        filter: ':not(.prev,.next)',
         hash: true,
         easing:'easeOutQuad',
         duration: 2000
+    });
+
+    $('ul', '#characters').localScroll({
+        target: '#characters ul',
+        axis: 'x',
+        hash: true,
+        easing:'easeOutQuint',
+        duration: 900
     });
 
     skrollr.init({
@@ -67,42 +76,5 @@
     // close navigation on touch devices when selectin an elemnt from the list
     $('.touch #cd-vertical-nav a').on('click', function () {
         navigationBlock.removeClass('open');
-    });
-
-    $('ul', '#characters').anoSlide({
-        items: 1,
-        speed: 900,
-        prev: 'a.prev',
-        next: 'a.next',
-        // lazy: true,
-        // auto: 4000,
-        onConstruct: function(instance) {
-            var paging = $('<div/>').addClass('paging fix').css({
-                position: 'absolute',
-                top: 1,
-                left:50 + '%',
-                width: instance.slides.length * 40,
-                marginLeft: -(instance.slides.length * 40) / 2
-            });
-
-            /* Build paging */
-            var onPagingClick = function () {
-                instance.stop().go($(this).data('index'));
-            };
-            for (var i = 0, l = instance.slides.length; i < l; i++) {
-                var a = $('<a/>').data('index', i).appendTo(paging).on('click', onPagingClick);
-
-                if (i === instance.current) {
-                    a.addClass('current');
-                }
-            }
-
-            instance.element.parent().append(paging);
-        },
-        onStart: function (ui) {
-            var paging = $('.paging');
-
-            paging.find('a').eq(ui.instance.current).addClass('current').siblings().removeClass('current');
-        }
     });
 }(jQuery, window, document));
